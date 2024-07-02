44a588177987403ea61b3f1a4fd4fd7cb23519ce
Pedro-Henrique-Chaves <165905499+Pedro-Henrique-Chaves@users.noreply.github.com>
2024-07-02 06:53:35 -0300

    Add files via upload

---
+++ JOGODAVELHACOELHO/app/src/main/java/com/example/jogodavelhacoelho/MusicService.kt
@@ -0,0 +1,35 @@
+package com.example.jogodavelhacoelho
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class MusicService : Service() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer.create(this, R.raw.musicafundo)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
